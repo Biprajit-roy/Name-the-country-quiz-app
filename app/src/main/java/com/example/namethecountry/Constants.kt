f9477847ffd89834b7ae92988af695d2a8bf793e
Biprajit Roy <biprajitroy11@gmail.com>
2022-08-06 09:54:05 +0530

    added widget

---
+++ app/src/main/java/com/example/namethecountry/Constants.kt
@@ -0,0 +1,16 @@
+package com.example.namethecountry
+
+import java.util.ArrayList
+
+object Constants{
+
+    fun getQuestions():ArrayList<Questions>{
+        val questionsList = ArrayList<Questions>()
+
+        val que1 = Questions(
+            1,"Which country the flag belong to?",
+        )
+    }
+
+
+}
